d6918e6862092ac4ab03d92de67016dc27d2b71a
Avishek Das <davishek7@gmail.com>
2020-11-19 00:26:42 +0530

    19th Nov Commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home,name='home'),
     path('products/', views.products,name='products'),
-    path('customer/', views.customer,name='customer')
+    path('customer/<str:pk>/', views.customer,name='customer'),
+
+    path('create_order/',views.createOrder,name='create_order'),
+    path('update_order/<str:pk>/',views.updateOrder,name='update_order')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import OrderForm
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders=Order.objects.all()
+    customers=Customer.objects.all()
+
+    total_customers=customers.count()
+    total_orders=orders.count()
+    delivered=orders.filter(status='Delivered').count()
+    pending=orders.filter(status='Pending').count()
+    
+    context={"orders":orders,"customers":customers,"total_orders":total_orders,"delivered":delivered,"pending":pending}
+    return render(request,'accounts/dashboard.html',context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products=Product.objects.all()
+    return render(request,'accounts/products.html',{"title":"Products Details","products":products})
 
-def customer(request):
-    return render(request,'accounts/customer.html')
+def customer(request,pk):
+    customer=Customer.objects.get(id=pk)
+    orders=customer.order_set.all()
+    order_count=orders.count()
+    context={"title":"Customer Details","customer":customer,"orders":orders,"order_count":order_count}
+    return render(request,'accounts/customer.html',context)
 
+def createOrder(request):
+    form=OrderForm()
+    if request.method=='POST':
+        form=OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context={"form":form}
+    return render(request,'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+
+    order=Order.objects.get(id=pk)
+    form=OrderForm(instance=order)
+    context={"form":form}
+    return render(request,'accounts/order_form.html',context)
