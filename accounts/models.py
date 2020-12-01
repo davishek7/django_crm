47ee54138204bceafec56c71f90a33cd5ce0cce9
Avishek Das <davishek7@gmail.com>
2020-12-01 18:30:03 +0530

    1st December 2020 commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
+    user=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     name=models.CharField(max_length=200,null=True)
     phone=models.CharField(max_length=200,null=True)
     email=models.CharField(max_length=200,null=True)
@@ -42,3 +44,7 @@ class Order(models.Model):
     product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,null=True)
     status=models.CharField(max_length=200,null=True,choices=STATUS)
+    note=models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.product.name
